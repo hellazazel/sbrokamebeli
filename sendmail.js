c9b1a885e32853332c5b4ce77527f370fc66738c
hellazazel <worcaut2014@gmail.com>
2024-06-18 18:03:21 +0300

    add_clean_in_project

--- sendmail.js
+++ sendmail.js
@@ -6,12 +6,12 @@
 //     });
 //     $('a[name=first_formBtn]').click(function(e) {
 //       e.preventDefault(); // Предотвращаем отправку формы по умолчанию
-  
+
 //     //   var formData = new FormData($('.homepage__form')[0]);
 //         let name = $("input[name=user__name1]").val()
 //         let tel = $("input[name=user__tel1]").val()
 //         let message = $("textarea[name=user__question1]").val()
-        
+
 //         // console.log(message)
 //         // return;
 //         $.ajax({
@@ -36,113 +36,113 @@
 //     });
 // });
 
-$(document).ready(function() {
+$(document).ready(function () {
   let F = null;
   let file = $("input[name=user__file1]");
-  file.change(function(){
+  file.change(function () {
     F = this.files[0];
-  })
-  
+  });
 
-  $('a[name=first_formBtn]').click(function(e) {
+  $("a[name=first_formBtn]").click(function (e) {
     e.preventDefault();
 
     let name = $("input[name=user__name1]").val();
     let tel = $("input[name=user__tel1]").val();
     let message = $("textarea[name=user__question1]").val();
 
-    if (name === '' || tel === '') {
+    if (name === "" || tel === "") {
       alert("Заполните поля ИМЯ и ТЕЛЕФОН!");
     } else {
       let formData = new FormData();
-      formData.append('name', name);
-      formData.append('tel', tel);
-      formData.append('message', message);
-    
-      if (F !== null) { // Если файл был выбран, добавьте его к formData
-        formData.append('F', F);
+      formData.append("name", name);
+      formData.append("tel", tel);
+      formData.append("message", message);
+
+      if (F !== null) {
+        // Если файл был выбран, добавьте его к formData
+        formData.append("F", F);
       }
 
-    $.ajax({
-      url: 'php/sendmail_1.php',
-      type: 'POST',
-      data: formData,
-      contentType: false,
-      processData: false,
-      success: function(response) {
-        alert(response);
-      },
-      error: function(xhr, status, error) {
-        // Обработка ошибки
-      }
-    });
+      $.ajax({
+        url: "php/sendmail_1.php",
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+          alert(response);
+        },
+        error: function (xhr, status, error) {
+          // Обработка ошибки
+        },
+      });
     }
   });
 
   let Fi = null;
   let file2 = $("input[name=user__file2]");
 
-  file2.change(function(){
+  file2.change(function () {
     Fi = this.files[0];
   });
 
-  $("a[name=sec__form]").click(function(e){
-    e.preventDefault()
-    
+  $("a[name=sec__form]").click(function (e) {
+    e.preventDefault();
+
     let checkboxes = [
-      checkbox_1 = $("#homepage__checkbox-1"),
-      checkbox_2 = $("#homepage__checkbox-2"),
-      checkbox_3 = $("#homepage__checkbox-3"),
-      checkbox_4 = $("#homepage__checkbox-4"),
-      checkbox_5 = $("#homepage__checkbox-5"),
-      checkbox_6 = $("#homepage__checkbox-6"),
-      checkbox_7 = $("#homepage__checkbox-7"),
-      checkbox_8 = $("#homepage__checkbox-8"),
-      checkbox_9 = $("#homepage__checkbox-9"),
-      checkbox_10 = $("#homepage__checkbox-10"),
-      checkbox_11 = $("#homepage__checkbox-11"),
-      checkbox_12 = $("#homepage__checkbox-12")
-    ]
+      (checkbox_1 = $("#homepage__checkbox-1")),
+      (checkbox_2 = $("#homepage__checkbox-2")),
+      (checkbox_3 = $("#homepage__checkbox-3")),
+      (checkbox_4 = $("#homepage__checkbox-4")),
+      (checkbox_5 = $("#homepage__checkbox-5")),
+      (checkbox_6 = $("#homepage__checkbox-6")),
+      (checkbox_7 = $("#homepage__checkbox-7")),
+      (checkbox_8 = $("#homepage__checkbox-8")),
+      (checkbox_9 = $("#homepage__checkbox-9")),
+      (checkbox_10 = $("#homepage__checkbox-10")),
+      (checkbox_11 = $("#homepage__checkbox-11")),
+      (checkbox_12 = $("#homepage__checkbox-12")),
+    ];
 
     let selectedCheckboxes = [];
 
-    $.each(checkboxes, function(index, checkbox) {
+    $.each(checkboxes, function (index, checkbox) {
       if (checkbox.is(":checked")) {
         selectedCheckboxes.push({
           checkbox: checkbox,
-          value: checkbox.data("value")
+          value: checkbox.data("value"),
         });
       }
     });
 
     // console.log(selectedCheckboxes[0].value);
-    let name = $("input[name=user__name2]").val()
-    let tel = $("input[name=user__tel2]").val()
-    let message = $("textarea[name=user__question2]").val()
+    let name = $("input[name=user__name2]").val();
+    let tel = $("input[name=user__tel2]").val();
+    let message = $("textarea[name=user__question2]").val();
 
-    if(name == '' || tel == '') {
-      alert("Заполните поля ИМЯ и ТЕЛЕФОН!")
+    if (name == "" || tel == "") {
+      alert("Заполните поля ИМЯ и ТЕЛЕФОН!");
     }
 
     let formData = new FormData();
-    formData.append('name', name);
-    formData.append('tel', tel);
-    formData.append('message', message);
-    formData.append('F', Fi);
-    formData.append('selectedCheckboxes', JSON.stringify(selectedCheckboxes));
+    formData.append("name", name);
+    formData.append("tel", tel);
+    formData.append("message", message);
+    formData.append("F", Fi);
+    formData.append("selectedCheckboxes", JSON.stringify(selectedCheckboxes));
 
     $.ajax({
-      url: 'php/sendmail2.php',
-      type: 'POST',
+      url: "php/sendmail2.php",
+      type: "POST",
       data: formData,
       contentType: false,
       processData: false,
-      success: function(response) {
+      success: function (response) {
         alert(response);
       },
-      error: function(xhr, status, error) {
+      error: function (xhr, status, error) {
         // Обработка ошибки
-      }
+      },
     });
   });
 });
